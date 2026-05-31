fbb7233ec2c2847a78aaad3608eee28742388fe1
mark-alegre01 <mark.anthony.alegre05@gmail.com>
2026-05-31 20:33:20 +0800

    Update install_internet.sh to also force DNS resolution on boot

--- install_internet.sh
+++ install_internet.sh
@@ -5,6 +5,10 @@ sysctl -w net.ipv4.ip_forward=1
 iptables -t nat -A POSTROUTING -o end0 -j MASQUERADE
 iptables -A FORWARD -i end0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -i wlan0 -o end0 -j ACCEPT
+rm -f /etc/resolv.conf
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
+echo "nameserver 8.8.4.4" >> /etc/resolv.conf
+systemctl restart dnsmasq
 exit 0
 EOF
 chmod +x /etc/rc.local
